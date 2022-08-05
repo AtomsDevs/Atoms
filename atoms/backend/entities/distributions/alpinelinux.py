158c17e002841bb455965a428fe150b8bcddc54b
mirkobrombin <send@mirko.pm>
2022-08-05 14:30:10 +0200

    sort alpine releases

--- atoms/backend/entities/distributions/alpinelinux.py
+++ atoms/backend/entities/distributions/alpinelinux.py
@@ -7,7 +7,7 @@ class AlpineLinux(AtomDistribution):
             distribution_id="alpinelinux", 
             name="Alpine Linux",
             logo="alpine-linux-symbolic",
-            releases=["3.16.0", "3.16.1"],
+            releases=["3.16.1", "3.16.0"],
             remote_structure="https://dl-cdn.alpinelinux.org/alpine/v{0}/releases/{1}/alpine-minirootfs-{2}-{1}.tar.gz",
             remote_hash_structure="https://dl-cdn.alpinelinux.org/alpine/v{0}/releases/{1}/alpine-minirootfs-{2}-{1}.tar.gz.sha256",
             remote_hash_type="sha256",
