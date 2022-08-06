5fb5facde3cfd33900e6aa23b4ea3bd2d024cd41
cat-master21 <96554164+cat-master21@users.noreply.github.com>
2022-08-06 14:54:00 -0700

    feat: add debian to distributions

---
+++ atoms/backend/entities/distributions/debian.py
@@ -0,0 +1,16 @@
+from atoms.backend.entities.distribution import AtomDistribution
+
+
+class Debian(AtomDistribution):
+    def __init__(self):
+        super().__init__(
+            distribution_id="debian", 
+            name="Debian Linux (stable)",
+            logo="debian-symbolic",
+            releases=["11",],
+            remote_structure="https://cloud.debian.org/images/cloud/bullseye/latest/debian-{0}-genericcloud-{1}.tar.xz",
+            remote_hash_structure="https://cloud.debian.org/images/cloud/bullseye/latest/SHA512SUMS",
+            remote_hash_type="sha512",
+            architectures={"x86_64": "amd64"},
+            root="root.x86_64"
+        )
