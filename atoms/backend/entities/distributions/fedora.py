aa3418822f840953779d9ee85c797181cef9883c
mirkobrombin <send@mirko.pm>
2022-08-07 14:54:33 +0200

    use fedora base image

--- atoms/backend/entities/distributions/fedora.py
+++ atoms/backend/entities/distributions/fedora.py
@@ -8,7 +8,7 @@ class Fedora(AtomDistribution):
             name="Fedora",
             logo="fedora-symbolic",
             releases=["36"],
-            remote_structure="https://github.com/mirkobrombin/linux-rootfs-images/releases/download/fedora-{0}/Fedora-Container-Minimal-Base-{0}.{1}.tar",
+            remote_structure="https://github.com/mirkobrombin/linux-rootfs-images/releases/download/fedora-{0}/Fedora-Container-Base-{0}.{1}.tar",
             remote_hash_structure="https://github.com/mirkobrombin/linux-rootfs-images/releases/download/fedora-{0}/SHA1SUMS",
             remote_hash_type="sha1",
             architectures={"x86_64": "x86_64"},
